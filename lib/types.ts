b97dbd59680242a1faeea39a92fbbc636ff4d020
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-10-15 15:59:54 -0400

    more type cleanup

--- lib/types.ts
+++ lib/types.ts
@@ -1,17 +1,10 @@
 import { type APIGatewayProxyEvent } from 'aws-lambda';
 
-type ProxyIntegrationParams = {
-  paths?: Record<string, string>;
-  routePath?: string;
-};
-
 type EventBody<T = unknown> = {
   body: T;
 };
 
-export type ProxyIntegrationEvent<T = unknown> = Omit<APIGatewayProxyEvent, 'body'> &
-  ProxyIntegrationParams &
-  EventBody<T>;
+export type ProxyIntegrationEvent<T = unknown> = Omit<APIGatewayProxyEvent, 'body'> & EventBody<T>;
 
 export type TestObject = {
   message: string;
